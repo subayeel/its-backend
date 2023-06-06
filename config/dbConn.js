59d69866d0aa5a811db61dcd4bd164b16b17f649
subayeel <abdullahsubayeel999@gmail.com>
2023-06-06 16:33:34 +0530

    env vulnerable

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {
+    await mongoose.connect("mongodb+srv://subayeel:Qatar_123@atlascluster.ubgdabo.mongodb.net/?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
