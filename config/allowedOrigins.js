a0d726a63547e2f6ca1b462e3f5e11f66fb2d9fc
subayeel <abdullahsubayeel999@gmail.com>
2023-07-05 14:19:41 +0530

    Cors Resolve

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://subayeel.github.io",
   "http://localhost:4200",
-  "https://its-webapp.vercel.app/",
+  "https://its-webapp.vercel.app",
 ];
 
 module.exports = allowedOrigins;
